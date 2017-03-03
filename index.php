20a85b12143dd31d880e478085b1e7ad0943bac5
vipul-verma <vipul.firebase@gmail.com>
2017-03-03 16:34:56 +0530

    update

--- index.php
+++ index.php
@@ -19,31 +19,31 @@
             	<div class="authSection onyxBg clearfix">
                 	<div class="col-md-12">
                     	<ul class="nav nav-pills nav-justified">
-                          <li><a href="#signup" aria-controls="signup" role="pill" data-toggle="pill" class="btn btn-default fs16 f600 btn-tab">Sign Up</a></li>
+                          <li><a href="#register" aria-controls="signup" role="pill" data-toggle="pill" class="btn btn-default fs16 f600 btn-tab">Register</a></li>
                           <li class="active"><a href="#login" aria-controls="login" role="pill" data-toggle="pill" class="btn btn-default fs16 f600 btn-tab">Log In</a></li>
                         </ul>
                         
                         <div class="tab-content">
-                            <div role="tabpanel" class="tab-pane whiteFg" id="signup">
+                            <div role="tabpanel" class="tab-pane whiteFg" id="register">
                             	<div class="login-form-heading text-center">
                                 	<h2>Fill in the details</h2>
                                 </div>
-                            	<form action="index.php" method="post" class="signup-form">
+                            	<form action="index.php" method="post" class="register-form">
                                 	  <div class="form-group">
                                         <label for="" class="f600">Full Name</label>
-                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Full Name *">
+                                        <input type="email" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Full Name *">
                                       </div>
                                 	  <div class="form-group">
                                         <label for="" class="f600">Email address</label>
-                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Email *">
+                                        <input type="email" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Email *">
                                       </div>
                                       <div class="form-group">
                                         <label for="" class="f600">Password</label>
-                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Password *">
+                                        <input type="password" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Password *">
                                       </div>
                                       <div class="form-group">
                                         <label for="" class="f600">Re-enter Password</label>
-                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Re-enter Password *">
+                                        <input type="password" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Re-enter Password *">
                                       </div>
                                       <button type="submit" class="btn btn-default btn-register f600 fs21" name="register">Register</button>
                                 </form>
@@ -55,11 +55,11 @@
                             	<form action="index.php" method="post" class="login-form">
                                 	  <div class="form-group">
                                         <label for="" class="f600">Email address</label>
-                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Email *">
+                                        <input type="email" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Email *">
                                       </div>
                                       <div class="form-group">
                                         <label for="" class="f600">Password</label>
-                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Password *">
+                                        <input type="password" class="form-control onyxBg bRad0 whiteFg fs16" id="" placeholder="Password *">
                                       </div>
                                       <div class="form-group">
                                         <p class="forgot"><a href="forgot.php" class="satinSheenGoldFg">Forgot Password?</a></p>
