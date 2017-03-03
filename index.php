9a7d85253471636af63db6660b6f6c5f878391d5
vipul-verma <vipul.firebase@gmail.com>
2017-03-03 16:11:41 +0530

    forms created
    signup/login

--- index.php
+++ index.php
@@ -15,7 +15,67 @@
 <body>
 	<div class="container">
 		<div class="row">
-			
+			<div class="col-md-5 col-center">
+            	<div class="authSection onyxBg clearfix">
+                	<div class="col-md-12">
+                    	<ul class="nav nav-pills nav-justified">
+                          <li><a href="#signup" aria-controls="signup" role="pill" data-toggle="pill" class="btn btn-default fs16 f600 btn-tab">Sign Up</a></li>
+                          <li class="active"><a href="#login" aria-controls="login" role="pill" data-toggle="pill" class="btn btn-default fs16 f600 btn-tab">Log In</a></li>
+                        </ul>
+                        
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane whiteFg" id="signup">
+                            	<div class="login-form-heading text-center">
+                                	<h2>Fill in the details</h2>
+                                </div>
+                            	<form action="index.php" method="post" class="signup-form">
+                                	  <div class="form-group">
+                                        <label for="" class="f600">Full Name</label>
+                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Full Name *">
+                                      </div>
+                                	  <div class="form-group">
+                                        <label for="" class="f600">Email address</label>
+                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Email *">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="" class="f600">Password</label>
+                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Password *">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="" class="f600">Re-enter Password</label>
+                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Re-enter Password *">
+                                      </div>
+                                      <button type="submit" class="btn btn-default btn-signup f600 fs21" name="login">Sign Up</button>
+                                </form>
+                            </div>
+                            <div role="tabpanel" class="tab-pane whiteFg active fade in" id="login">
+                            	<div class="login-form-heading text-center">
+                                	<h2>Welcome Back !</h2>
+                                </div>
+                            	<form action="index.php" method="post" class="login-form">
+                                	  <div class="form-group">
+                                        <label for="" class="f600">Email address</label>
+                                        <input type="email" class="form-control onyxBg bRad0" id="" placeholder="Email *">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="" class="f600">Password</label>
+                                        <input type="password" class="form-control onyxBg bRad0" id="" placeholder="Password *">
+                                      </div>
+                                      <div class="form-group">
+                                        <p class="forgot"><a href="forgot.php" class="satinSheenGoldFg">Forgot Password?</a></p>
+                                      </div>
+                                      <button type="submit" class="btn btn-default btn-login f600 fs21" name="login">Log In</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                    	<div class="formBucket">
+                        	
+                        </div>
+                    </div>
+                </div>
+            </div>	
 		</div>
 	</div>
 
