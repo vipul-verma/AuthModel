774c9c6d164d1ef5f1ae2a09f632b0f6a4a3ec89
vipul-verma <vipul.firebase@gmail.com>
2017-03-03 12:13:29 +0530

    Base upload
    all files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600,700" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/style.css">
+        <title>Auth</title>
+    </head>
+<body>
+	<div class="container">
+		<div class="row">
+			
+		</div>
+	</div>
+
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
